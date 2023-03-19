30101ad085ca078d00abe33abc44a8c91de060cd
romaoplays <romaoplays@hotmail.com>
2023-03-19 10:30:03 -0300

    Fixed sorting bug and added total video length sum

--- video_len_checker.py
+++ video_len_checker.py
@@ -13,7 +13,13 @@ for i in range(len(files)):
    if files[i].lower().endswith(video_extensions):
         complete_files.append(files[i])
 
-for i in range(len(complete_files)):
-    clip = VideoFileClip(files[i])
-    print(f"{files[i]} - {time.strftime('%H:%M:%S', time.gmtime(clip.duration))}")
+complete_files = sorted(complete_files)
+total_duration = 0
 
+for i in range(len(complete_files)):
+    clip = VideoFileClip(complete_files[i])
+    total_duration += clip.duration
+    print(f"{complete_files[i]} - {time.strftime('%H:%M:%S', time.gmtime(clip.duration))}")
+
+
+print(f"---- Total Length: {time.strftime('%H:%M:%S', time.gmtime(total_duration))} ----")
